b56b8c88309255cac18e5d8f8973fbb0b578ad55
yue <1263887632@qq.com>
2023-05-18 17:22:15 +0800

    feat: 增加高德定位功能

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,88 @@
-import 'package:cola_weather/page_cities.dart';
+import 'dart:async';
+
+import 'package:amap_flutter_location/amap_flutter_location.dart';
+import 'package:amap_flutter_location/amap_location_option.dart';
 import 'package:cola_weather/page_home.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, Object>? _locationResult;
+
+  StreamSubscription<Map<String, Object>>? _locationListener;
+
+  AMapFlutterLocation _locationPlugin = new AMapFlutterLocation();
+
+  @override
+  void initState() {
+    super.initState();
+    AMapFlutterLocation.updatePrivacyShow(true, true);
+    AMapFlutterLocation.updatePrivacyAgree(true);
+    AMapFlutterLocation.setApiKey(
+        "30fef395569d893d935302f2e9d9f4b3", "iOS_KEY");
+
+    requestPermission();
+
+    _locationListener = _locationPlugin
+        .onLocationChanged()
+        .listen((Map<String, Object> result) {
+      setState(() {
+        _locationResult = result;
+        print("_locationResult : $_locationResult");
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (null != _locationListener) {
+      _locationListener?.cancel();
+    }
+    _stopLocation();
+    _locationPlugin.destroy();
+  }
+
+  void _setLocationOption() {
+    AMapLocationOption locationOption = new AMapLocationOption();
+
+    locationOption.onceLocation = false;
+
+    locationOption.needAddress = true;
+
+    locationOption.geoLanguage = GeoLanguage.DEFAULT;
+
+    locationOption.desiredLocationAccuracyAuthorizationMode =
+        AMapLocationAccuracyAuthorizationMode.ReduceAccuracy;
+
+    locationOption.fullAccuracyPurposeKey = "AMapLocationScene";
+
+    locationOption.locationInterval = 2000;
+    locationOption.locationMode = AMapLocationMode.Hight_Accuracy;
+    locationOption.distanceFilter = -1;
+    locationOption.desiredAccuracy = DesiredAccuracy.Best;
+    locationOption.pausesLocationUpdatesAutomatically = false;
+    _locationPlugin.setLocationOption(locationOption);
+  }
+
+  void _startLocation() {
+    _setLocationOption();
+    _locationPlugin.startLocation();
+  }
+
+  void _stopLocation() {
+    _locationPlugin.stopLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -15,5 +91,35 @@ class MyApp extends StatelessWidget {
       },
       home: HomePage(),
     );
+  }
+
+  /// 动态申请定位权限
+  void requestPermission() async {
+    // 申请权限
+    bool hasLocationPermission = await requestLocationPermission();
+    if (hasLocationPermission) {
+      print("定位权限申请通过");
+      _startLocation();
+    } else {
+      print("定位权限申请不通过");
+    }
+  }
+
+  /// 授予定位权限返回true， 否则返回false
+  Future<bool> requestLocationPermission() async {
+    //获取当前的权限
+    var status = await Permission.location.status;
+    if (status == PermissionStatus.granted) {
+      //已经授权
+      return true;
+    } else {
+      //未授权则发起一次申请
+      status = await Permission.location.request();
+      if (status == PermissionStatus.granted) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }
